4c9af218df818630a2bc7903c24308a6c73760d5
mayank7enn <96970037+mayank7enn@users.noreply.github.com>
2024-07-20 23:21:12 +0530

    Update LeftNav.jsx

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -54,7 +54,7 @@ const LeftNav = () => {
                 })}
                 <hr className="my-5 border-white/[0.2]" />
                 <div className="text-white/[0.5] text-[12px]">
-                    Clone by: JS Dev Hindi
+                    Clone by: Mayank Sharma
                 </div>
             </div>
         </div>
